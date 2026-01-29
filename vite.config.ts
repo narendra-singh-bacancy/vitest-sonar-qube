c7eba64afb4e4082404264f92c8243136f9089a0
Narendra Singh <narendra.singh@bacancy.com>
2026-01-29 12:54:17 +0530

    feat: initial commit for vitest-sonar-qube project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
+  },
+});
