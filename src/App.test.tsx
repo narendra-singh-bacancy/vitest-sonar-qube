39478edbf0d6674eee9bd680a4cc9812b5a413f9
Narendra Singh <narendra.singh@bacancy.com>
2026-01-29 14:00:27 +0530

    chng: minor changes to run the workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-    it('renders hello world', () => {
+    it('renders Hello Narendra', () => {
         render(<App />);
-        expect(screen.getByText('Hello World')).toBeInTheDocument();
+        expect(screen.getByText('Hello Narendra')).toBeInTheDocument();
     });
 });
